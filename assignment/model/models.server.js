50219098a0094e81d4272d04d24dbd1963408f9d
cassielock <chen.keyu@husky.neu.edu>
2019-04-15 00:40:45 -0700

    hw5

--- assignment/model/models.server.js
+++ assignment/model/models.server.js
@@ -5,6 +5,6 @@ var mongoose = require('mongoose');
 // var db = mongoose.connect('mongodb://localhost:27017/webdev');
 
 // heroku
- var db = mongoose.connect('mongodb://<KarlieC>:<Cky@2018>@ds137596.mlab.com:37596/heroku_zvkvr8m3')
+ var db = mongoose.connect('mongodb://KarlieC:Cky@2018@ds137596.mlab.com:37596/heroku_zvkvr8m3')
 
 module.exports = db;
